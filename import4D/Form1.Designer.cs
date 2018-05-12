b45049a5be9185a863bcab397e4f8b185a0096c5
x3titan <31681151+x3titan@users.noreply.github.com>
2018-05-13 01:49:56 +0800

    添加项目文件。

---
+++ import4D/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace import4D {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 371);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(693, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(693, 86);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(26, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 86);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(693, 285);
+            this.panel2.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(693, 285);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 393);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ import4D/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Odbc;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace import4D {
+    public partial class Form1 : Form {
+        private string odbcConnectionString;
+        private OdbcConnection odbcConnection = new OdbcConnection();
+        private TamPubWin1.LogFile log = new TamPubWin1.LogFile();
+        private bool forceExit = false;
+
+        public Form1() {
+            InitializeComponent();
+            log.listBox = listBox1;
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            testLoad();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            string configFile = TamPub1.FileOperation.currentFilePath + "config.xml";
+            TamPubWin1.Etc.loadDesktop(configFile, this);
+            odbcConnectionString = TamPub1.ConfigFileXml.readString(configFile, "odbcConnectionString", "DSN=Peijia;UID=sa;PWD=zaq12wsx;");
+            //textBox1.Text = TamPub1.ConfigFileXml.readString(configFile, "textBox1Text", "0");
+            timer1.Enabled = true;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) {
+            string configFile = TamPub1.FileOperation.currentFilePath + "config.xml";
+            TamPubWin1.Etc.saveDesktop(configFile, this);
+            //TamPub1.ConfigFileXml.writeString(configFile, "textBox1Text", textBox1.Text);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+
+        }
+
+        private bool testLoad() {
+            string filename = "D:\\tam\\project\\nokiaBigData\\s1u_gen_konw_apn\\20180425_known_apn_s1u_general.csv";
+            System.IO.FileStream fs = null;
+            System.IO.BinaryReader reader;
+
+            if (!System.IO.File.Exists(filename)) {
+                log.writeLogWarning("文件不存在, filename=" + filename);
+                return false;
+            }
+            try {
+                fs = new System.IO.FileStream(filename, System.IO.FileMode.Open);
+            } catch (Exception ex) {
+                log.writeLogWarning("无法读取文件,reason="+ ex.Message +", filename=" + filename);
+                return false;
+            }
+            reader = new System.IO.BinaryReader(fs, System.Text.Encoding.GetEncoding("GBK"));
+
+
+            //===============================
+            //0:xDR_ID 一个会话生成一个xDR ID。>> 无用
+            //1:IMSI 用户IMSI（TBCD编码） >> 15位
+            //2:MSISDN 用户号码（TBCD编码）>> 手机号,13-??位
+            //3:Cell_ID UE所在小区的ECI >> 整数？
+            //4:APN APN >> ??
+            //5:App_Type_Code 业务类型编码，参见附录D XDR类型编码定义 >> 要个编码表
+            //6:Procedure_Start_Time TCP/ UDP流开始时间，UTC时间 - 毫秒 >> 需要解码时间
+            //7:Procedure_End_Time TCP/ UDP流结束时间，UTC时间 - 毫秒 >> 需要解码时间
+            //8:App_Type 应用大类—集团18种应用大类，参见《数据流量业务大类分类》 >> 要个编码表
+            //9:App_Sub_type 应用小类 >> 要个编码规则
+            //10:USER_IPv4 终端用户的IPv4地址 >> 7-15长度
+            //11:User_Port 用户的四层端口号 >> 谁是服务器，反向连接怎么表示？
+            //12:L4_protocal L4协议类型：0：TCP；1：UDP >> 1位长度
+            //13:App_Server_IP_IPv4  访问服务器的IPv4地址 >> 7-15位长度
+            //14:App_Server_Port 访问的服务器的端口 >> 谁是服务器，反向连接怎么表示？
+            //15:UL_Data 上行流量；单位：字节 >> 整数
+            //16:DL_Data 下行流量；单位：字节 >> 整数
+            //17:UL_IP_Packet    上行IP包数 >> 整数
+            //18:DL_IP_Packet    下行IP包数 >> 整数
+            //19:TCP_Try_Cnt TCP建链尝试次数，一次TCP流多次SYN的数值; 非TCP传输时，此字段填0 >> 整数
+            //20:TCP_Link_flag   TCP连接状态指示；0：成功；1：失败 >> 255代表什么？
+            //21:Talk_if_end 会话是否结束标志；1：结束 （用户中断）2：未结束   3：超时  4: 拆分会话的第一话单   5：结束 （服务端中断） >> 整数
+            //===============================
+            //                   0              1               2       3       4                             5       6           7           8  9   10             11    12 13              14   15   16 17 18 19 20  21                  
+            //00008c10045a0e95c498,460041238803470,861064723883470,141986486,cmiotfz.sh.mnc004.mcc460.gprs,  100,1524588437627,1524588437683,21,8921,10.4.38.181,   0,    1, 114.114.114.114,0,   84,  84,1, 1, 0, 255,3
+            //00008810545a3772ea0e,460027172840010,8613484491206,  242843531,temp.sn.mnc002.mcc460.gprs,     100,1524598898676,1524598913716,21,143, 100.6.132.46,  47680,0, 223.202.195.96, 443, 300, 0, 5, 0, 5, 1,  2
+            //00008810545a3772ea00,460040096750197,861064851419087,7527307,  cmiotbyda.sn.mnc004.mcc460.gprs,100,1524598898656,1524598898695,8, 7549,10.246.119.29, 53661,0, 192.168.0.250,  5000,52,  52,1, 1, 0, 1,  2
+            //00008810745a37722dd5,460008072539994,8613758069490,  28010507, OTA.ZJ.mnc000.mcc460.gprs,      100,1524598898731,1524598906076,13,40,  104.13.201.144,49839,0, 106.11.248.1,   80,  120, 0, 2, 0, 2, 1,  2
+            //00008210545a67994584,460002605393612,8613927041963,  141269025,BOSSTJFT.GD.mnc000.mcc460.gprs, 100,1524611225191,1524611225191,12,1,   10.144.76.65,  52318,0, 123.151.10.164, 443, 1215,0, 1, 0, 0, 1,  2
+
+            char[] buff = reader.ReadChars(1000);
+
+            log.writeLogCommon(new string(buff));
+
+            //大方向：读取入库至apnDay180
+            
+            //10. 初始化内存手机表
+            
+            //20. 扫描文件获取手机号列表
+
+            //30. 根据手机号列表创建apnDay180框架
+
+            //35. 获取当前日期序列号
+
+            //40. 扫描每行产生update
+
+            //50. 更新当前日期序列号
+
+            //100. 完成
+
+
+            fs.Close();
+            return true;
+        }
+    }
+}
